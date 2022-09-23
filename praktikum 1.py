ff2ceb0edfd654c3120592c851529ffb34b39128
hafizharp <114157840+hafizharp@users.noreply.github.com>
2022-09-23 10:49:42 +0700

    Add files via upload

---
+++ praktikum 1.py
@@ -0,0 +1,7 @@
+
+
+print("menghitung luas ruangan")
+panjang = float(input("masukkan panjang (meter): "))
+lebar = float(input("masukkan lebar (meter): "))
+luas = int(panjang*lebar)
+print("luas ruangan: ", luas, "meter")
